53ecd95dc24e9d815b045944af1a572b0c53d358
EiffelIKE <jctrujillo759@gmail.com>
2022-08-10 02:01:31 -0700

    userSlice created and added to the store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import UserList from './components/UserList'
+// redux
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 const App = () => {
   return (
-    <>
+    <Provider store={store} >
       <UserList/>
-    </>
+    </Provider>
   )
 }
 
